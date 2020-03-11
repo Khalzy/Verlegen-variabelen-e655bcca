2088f89e0e4a3b18df3ae7c05025b23c58100409
Candace <2100978@talnet.nl>
2020-03-11 11:35:14 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let result;
 let text;
 
 function calculate() {
-  text = "omtrek:";
+  text = "omtrek: ";
   numWaarde = document.getElementById("num").value;
   result = pi * numWaarde;
   document.getElementById("output").innerText = text + result;
 
   {
-    text = "oppervlakte:";
+    text = "oppervlakte: ";
     numWaarde = (numWaarde * 1) / 2;
     result = numWaarde * numWaarde * pi;
     document.getElementById("output1").innerText = text + result;

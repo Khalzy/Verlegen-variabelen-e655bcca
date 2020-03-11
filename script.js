3744e618339ed9c584ad1775743f6b5a8545281b
Candace <2100978@talnet.nl>
2020-03-11 11:32:16 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function calculate() {
   document.getElementById("output").innerText = result;
 
   {
-    numWaarde = numWaarde * 0.5;
+    numWaarde = (numWaarde * 1) / 2;
     result = numWaarde * numWaarde * pi;
     document.getElementById("output1").innerText = result;
   }
